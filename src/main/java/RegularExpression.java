bbea7704cd6f72b7de21bb1fc365e3b424df0502
Keven Huang <keven.huang@calix.com>
2014-12-05 17:18:47 +0800

    test

--- src/main/java/RegularExpression.java
+++ src/main/java/RegularExpression.java
@@ -25,10 +25,12 @@ public class RegularExpression
 	
 	public static void main(String[] args)
 	{
-		findInteger();
-		compilePattern();
-		replaceSpace() ;
-		testPrime();
+//		findInteger();
+//		compilePattern();
+//		replaceSpace() ;
+//		testPrime();
+//		backreferences();
+	    testNetconf();
 	}
 
 	private static void compilePattern()
@@ -68,6 +70,54 @@ public class RegularExpression
 		 
 		public static boolean prime(int n) {
 		  return !new String(new char[n]).matches(".?|(..+?)\\1+");
+		}
+		
+		
+		public static void backreferences() {
+		    String regex = "(test)kk\\1";
+		    Pattern p = Pattern.compile(regex);
+		        Matcher m = p.matcher("mmtestkktestkkk");
+		        while (m.find()) {
+		            System.out.println(m.group());
+		        }
+		        System.out.println(m.matches() );
+		    
+		}
+		
+		public static void testNetconf() {
+		    String netconf = " <error-tag>data-missing</error-tag>"
+		            + "<error-severity>error</error-severity>"
+		            + "<nc:error-path xmlns:base=\"http://www.calix.com/ns/exa/base\" >"
+		            +"\n"
+		            +"/nc:rpc/nc:edit-config/nc:config/base:config/base:profile/base:match-list[base:name='test_matchList']"
+		            
+		            +"\n"
+		            + " </nc:error-path>" + "\n" +
+		            "<error-info>" +"\n" 
+		            + "<bad-element>match-list</bad-element>"
+		            +"\n"
+		            + "</error-info>"
+		            +"\n"
+		            + "</rpc-error>";
+		    
+		    String regex = "<error-tag>(?<tag>.*)</error-tag>|<error-app-tag>(?<apptag>.*)</error-app-tag>|<nc:error-path.*?>(?<path>.*)</nc:error-path>|<error-message.*?>(?<message>.*)</error-message>|<error-info.*?>(?<info>.*)</error-info>";
+
+	            Pattern p = Pattern.compile(regex, Pattern.DOTALL);
+	                Matcher m = p.matcher(netconf);
+	                int index = 1;
+	                while (m.find()) {
+	                    System.out.println(m.group());
+	                        System.out.println(m.group(1));
+	                        System.out.println(m.group(2));
+	                        System.out.println(m.group(3));
+	                        System.out.println("===========");
+	                        System.out.println(m.group("path"));
+	                            System.out.println(m.group("tag"));
+	                            System.out.println(m.group("info"));
+
+	                            System.out.println("===========");
+	                            System.out.println("===========");
+	                }
 		}
 
 }
